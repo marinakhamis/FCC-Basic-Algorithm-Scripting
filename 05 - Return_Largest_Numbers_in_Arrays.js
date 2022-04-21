e8c6f477f7f64d5b68450aad9be5c7e20dcbd09e
Marina khamis <35566244+marinakhamis@users.noreply.github.com>
2022-04-21 17:40:46 +0200

    Update 05 - Return_Largest_Numbers_in_Arrays.js

--- 05 - Return_Largest_Numbers_in_Arrays.js
+++ 05 - Return_Largest_Numbers_in_Arrays.js
@@ -6,10 +6,18 @@ For simplicity, the provided array will contain exactly 4 sub-arrays.
 Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
 */
+
 function largestOfFour(arr) {
+  // First: We initialize an empty array to store the sorted arrays in
   let largestArr = []
+  // I mapped the array of arrays in order to sort each one of them 
   arr.map((a)=> {
+  // used sort in order to sort arrays 
+    // Note: (b-a) => will sort descendingly 
+    // (a-b) => will sort ascendingly 
+    // https://www.w3schools.com/jsref/jsref_sort.asp
    a.sort((a,b) => {return b-a})
+    // push the first (largest) number in the largestArr
    largestArr.push(a[0])
   })
   return largestArr
